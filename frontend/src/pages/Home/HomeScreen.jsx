5cf36932931ecd797015cd670a558a3b520126f9
Priyanshu Singh <priyanshusinghdev03@gmail.com>
2025-01-29 15:09:42 +0530

    navbar

--- frontend/src/pages/Home/HomeScreen.jsx
+++ frontend/src/pages/Home/HomeScreen.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { useAuthStore } from "../../store/authUser";
+import Navbar from "../../components/Navbar";
 
 const HomeScreen = () => {
   const { logout } = useAuthStore();
   return (
     <div>
-      <button onClick={() => logout()}>Logout</button>
+      <Navbar />
     </div>
   );
 };
